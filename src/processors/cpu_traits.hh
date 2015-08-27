24d85511dfd12ad7f252e55e85ec50adc53ae68e
Franck Michea <franck.michea@gmail.com>
2015-08-27 10:14:14 +0200

    Add function to print an abstract address type from a CPU traits type.

--- src/processors/cpu_traits.hh
+++ src/processors/cpu_traits.hh
@@ -2,7 +2,16 @@
 #ifndef JARBOCUL_PROCESSORS_CPU_TRAITS_HH_
 # define JARBOCUL_PROCESSORS_CPU_TRAITS_HH_
 
+# include <iomanip>
+# include <sstream>
+# include <string>
+
 template <typename CPU>
 struct cpu_traits {};
+
+template <typename CPU>
+std::string addr2str(typename cpu_traits<CPU>::AddrType pc);
+
+# include "cpu_traits.hxx"
 
 #endif /* !JARBOCUL_PROCESSORS_CPU_TRAITS_HH_ */
---
+++ src/processors/cpu_traits.hxx
@@ -0,0 +1,17 @@
+#ifndef CPU_TRAITS_HXX_
+# define CPU_TRAITS_HXX_
+
+# include "cpu_traits.hh"
+
+template <typename CPU>
+std::string addr2str(typename cpu_traits<CPU>::AddrType pc) {
+    std::ostringstream addr;
+
+    uint16_t addr_width = sizeof (typename cpu_traits<CPU>::AddrType) * 2;
+
+    addr << std::setfill('0') << std::setw(addr_width) << std::hex
+         << std::uppercase << pc;
+    return addr.str();
+}
+
+#endif /* !CPU_TRAITS_HXX_ */
